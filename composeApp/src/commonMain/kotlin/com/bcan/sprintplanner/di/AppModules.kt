7ef6d190684ca8ee0cd033dc6c4e032f3246bf07
burak.gorgulu <burak.gorgulu@beymen.com>
2024-11-01 09:51:00 +0300

    added repository and viewmodel modules

--- composeApp/src/commonMain/kotlin/com/bcan/sprintplanner/di/AppModules.kt
+++ composeApp/src/commonMain/kotlin/com/bcan/sprintplanner/di/AppModules.kt
@@ -1,9 +1,22 @@
 package com.bcan.sprintplanner.di
 
+import com.bcan.sprintplanner.data.repositories.HomeRepository
+import com.bcan.sprintplanner.data.repositories.HomeRepositoryImpl
+import com.bcan.sprintplanner.presentation.home.HomeViewModel
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.firestore.firestore
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
-val firebaseModule = module {
+val provideFirebaseModule = module {
     single { Firebase.firestore }
+}
+
+val provideRepositoryBindModules = module {
+    factoryOf(::HomeRepositoryImpl) { bind<HomeRepository>() }
+}
+
+val provideViewModelModule = module {
+    factoryOf(::HomeViewModel)
 }
