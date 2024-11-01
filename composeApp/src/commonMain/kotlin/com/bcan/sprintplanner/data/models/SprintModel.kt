3a0163dc760c1cdf9fea6f3a2d75b4ec257848ac
burak.gorgulu <burak.gorgulu@beymen.com>
2024-11-01 09:52:51 +0300

    created models

---
+++ composeApp/src/commonMain/kotlin/com/bcan/sprintplanner/data/models/SprintModel.kt
@@ -0,0 +1,16 @@
+package com.bcan.sprintplanner.data.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SprintModel(
+    val sprintId: String? = null,
+    val tasks: List<TaskModel>? = null
+)
+
+@Serializable
+data class TaskModel(
+    val taskId: String? = null,
+    val summary: String? = null,
+    val platform: String? = null,
+)
