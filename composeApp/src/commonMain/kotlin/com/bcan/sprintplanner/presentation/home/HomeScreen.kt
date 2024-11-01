578c305a5f2d96c7d915ddef79912763643f8814
burak.gorgulu <burak.gorgulu@beymen.com>
2024-11-01 09:52:58 +0300

    test datas

--- composeApp/src/commonMain/kotlin/com/bcan/sprintplanner/presentation/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/bcan/sprintplanner/presentation/home/HomeScreen.kt
@@ -1,11 +1,23 @@
 package com.bcan.sprintplanner.presentation.home
 
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.koinScreenModel
 
 class HomeScreen : Screen {
     @Composable
     override fun Content() {
+        val viewModel = koinScreenModel<HomeViewModel>()
 
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+        if(uiState.isLoading) CircularProgressIndicator()
+
+        Text(uiState.sprints.toString())
+        Text(uiState.tasks.toString())
     }
 }
