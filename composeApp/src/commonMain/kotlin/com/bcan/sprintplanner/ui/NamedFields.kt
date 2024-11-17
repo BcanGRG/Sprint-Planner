bacb95600fa73c37b1f3f810446973a2766fce22
burak.gorgulu <burak.gorgulu@beymen.com>
2024-11-17 13:30:49 +0300

    created filter fields

--- composeApp/src/commonMain/kotlin/com/bcan/sprintplanner/ui/NamedFields.kt
+++ composeApp/src/commonMain/kotlin/com/bcan/sprintplanner/ui/NamedFields.kt
@@ -1,11 +1,15 @@
 package com.bcan.sprintplanner.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExposedDropdownMenuBox
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -135,5 +139,107 @@ fun NamedTextField(
             onValueChange = onValueChange,
             modifier = Modifier.weight(7f)
         )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PlatformFilterDropdownField(
+    modifier: Modifier = Modifier,
+    value: String,
+    values: List<PlatformTypes>,
+    onClickDropdownItem: (PlatformTypes) -> Unit,
+) {
+
+    var isDropDownExpanded by remember { mutableStateOf(false) }
+
+    Column {
+        Text(
+            modifier = Modifier.padding(start = 3.dp, bottom = 3.dp),
+            text = "Platform",
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary
+        )
+        ExposedDropdownMenuBox(
+            modifier = modifier.width(150.dp),
+            expanded = isDropDownExpanded,
+            onExpandedChange = { isDropDownExpanded = !isDropDownExpanded }
+        ) {
+            RoundedSelectionCard(
+                value = value,
+                onClick = { isDropDownExpanded = true }
+            )
+            DropdownMenu(
+                modifier = Modifier.exposedDropdownSize(),
+                expanded = isDropDownExpanded,
+                onDismissRequest = { isDropDownExpanded = false },
+            ) {
+                values.forEach {
+                    DropdownMenuItem(
+                        enabled = true,
+                        onClick = {
+                            onClickDropdownItem(it)
+                            isDropDownExpanded = false
+                        }) {
+                        Text(
+                            text = it.name,
+                            fontSize = 15.sp,
+                            lineHeight = 20.sp, color = Color.Black
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AssigneeFilterDropdownField(
+    modifier: Modifier = Modifier,
+    value: String,
+    values: List<String>,
+    onClickDropdownItem: (String) -> Unit,
+) {
+
+    var isDropDownExpanded by remember { mutableStateOf(false) }
+
+    Column {
+        Text(
+            modifier = Modifier.padding(start = 3.dp, bottom = 3.dp),
+            text = "Assigned To",
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary
+        )
+        ExposedDropdownMenuBox(
+            modifier = modifier.width(150.dp),
+            expanded = isDropDownExpanded,
+            onExpandedChange = { isDropDownExpanded = !isDropDownExpanded }
+        ) {
+            RoundedSelectionCard(
+                value = value,
+                onClick = { isDropDownExpanded = true }
+            )
+            DropdownMenu(
+                modifier = Modifier.exposedDropdownSize(),
+                expanded = isDropDownExpanded,
+                onDismissRequest = { isDropDownExpanded = false },
+            ) {
+                values.forEach {
+                    DropdownMenuItem(
+                        enabled = true,
+                        onClick = {
+                            onClickDropdownItem(it)
+                            isDropDownExpanded = false
+                        }) {
+                        Text(
+                            text = it,
+                            fontSize = 15.sp,
+                            lineHeight = 20.sp, color = Color.Black
+                        )
+                    }
+                }
+            }
+        }
     }
 }
